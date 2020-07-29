266ac263e0197b760f8ba356a4fc5563404630bd
Stefanc23 <stefano.chr23@gmail.com>
2020-07-29 20:39:26 +0700

    Add changes to navbar and footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Footer.css";
+import tmdb from "./tmdb.png";
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__copyright">
-        &copy; {new Date().getFullYear()} Made with ❤️ by Stefano Christian W.
+      <div className="footer__center">
+        <img src={tmdb} alt="powered by TMDB" />
+        <span>
+          &copy; {new Date().getFullYear()} Made with ❤️ by{" "}
+          <a
+            className="footer__link"
+            href="https://github.com/Stefanc23/Netflix-Clone"
+          >
+            Stefano Christian W.
+          </a>
+        </span>
       </div>
     </footer>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -16,18 +16,35 @@ function Nav() {
   }, []);
 
   return (
-    <div className={`nav ${show && "nav__black"}`}>
+    <nav className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt="Netflix Logo"
       />
+      <ul className="nav__menu">
+        <a className="nav__link" href="#home">
+          Home
+        </a>
+        <a className="nav__link" href="#shows">
+          TV Shows
+        </a>
+        <a className="nav__link" href="#movies">
+          Movies
+        </a>
+        <a className="nav__link" href="#recent">
+          Recently Added
+        </a>
+        <a className="nav__link" href="#list">
+          My List
+        </a>
+      </ul>
       <img
         className="nav__avatar"
         src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
         alt="Netflix Avatar Icon"
       />
-    </div>
+    </nav>
   );
 }
 
